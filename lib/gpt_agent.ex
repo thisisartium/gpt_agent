0de53fc452fff455e84851f51b3dbc66f0a1efdf
John Wilger <john@johnwilger.com>
2023-12-14 22:03:33 -0800

    Fixes build errors. Oops

--- lib/gpt_agent.ex
+++ lib/gpt_agent.ex
@@ -5,10 +5,18 @@ defmodule GptAgent do
 
   use GenServer
 
+  @doc """
+  Initializes the GPT Agent
+  """
+  @spec init(any()) :: {:ok, any()}
   def init(init_arg) do
     {:ok, init_arg}
   end
 
+  @doc """
+  Starts the GPT Agent
+  """
+  @spec start_link() :: {:ok, pid()} | {:error, reason :: term()}
   def start_link do
     GenServer.start_link(__MODULE__, nil)
   end
--- test/support/test_case.ex
+++ test/support/test_case.ex
@@ -1,4 +1,8 @@
 defmodule GptAgent.TestCase do
+  @moduledoc """
+  This module provides test case template for GptAgent tests.
+  """
+
   use ExUnit.CaseTemplate
 
   using do
