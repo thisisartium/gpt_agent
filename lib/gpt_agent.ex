529a5c9a2eee218f236f0223ad1cb9394e84ad5f
John Wilger <john@johnwilger.com>
2023-12-18 10:31:50 -0800

    Go back to using cast for adding user message
    We already get the event when the message is added. I think I would rather allow
    the agent to continue to have messages added; they will just be moved to the end
    of the process queue if the thread is not in a state that allows adding messages
    (i.e. run in progress)

--- lib/gpt_agent.ex
+++ lib/gpt_agent.ex
@@ -26,7 +26,7 @@ defmodule GptAgent do
 
   defp ok(state, next), do: {:ok, state, next}
   defp noreply(state), do: {:noreply, state}
-  defp reply(state, reply, next), do: {:reply, reply, state, next}
+  defp noreply(state, next), do: {:noreply, state, next}
 
   defp send_callback(state, callback) do
     send(state.callback_handler, {__MODULE__, state.pid, callback})
@@ -82,7 +82,7 @@ defmodule GptAgent do
 
   defp heartbeat_interval_ms, do: Application.get_env(:gpt_agent, :heartbeat_interval_ms, 1000)
 
-  def handle_call({:add_user_message, message}, _caller, state) do
+  def handle_cast({:add_user_message, message}, state) do
     {:ok, message} = NonblankString.new(message)
 
     {:ok, %{body: %{"id" => id}}} =
@@ -94,7 +94,7 @@ defmodule GptAgent do
       thread_id: state.thread_id,
       content: message
     })
-    |> reply(:ok, {:continue, :run})
+    |> noreply({:continue, :run})
   end
 
   def handle_info({:check_run_status, id}, state) do
@@ -149,6 +149,6 @@ defmodule GptAgent do
   end
 
   def add_user_message(pid, message) do
-    GenServer.call(pid, {:add_user_message, message})
+    GenServer.cast(pid, {:add_user_message, message})
   end
 end
