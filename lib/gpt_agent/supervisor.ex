65c5185b62f9d48f3046cc0397432fd11f911fbd
John Wilger <john@johnwilger.com>
2024-01-03 17:51:26 -0800

    Add a dynamic supervisor to the library

---
+++ lib/gpt_agent/supervisor.ex
@@ -0,0 +1,16 @@
+defmodule GptAgent.Supervisor do
+  @moduledoc """
+  Manages the GptAgent processes, ensuring that there is only one process per thread-id.
+  """
+
+  use DynamicSupervisor
+
+  def start_link(init_arg) do
+    DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl DynamicSupervisor
+  def init(_init_arg) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+end
