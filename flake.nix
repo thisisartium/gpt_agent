e9829d743fca447a03c8b347eb62121b80eb9590
John Wilger <john@johnwilger.com>
2024-10-08 15:48:29 -0700

    Update devenv to use nix

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A flake for building development environment of Phoenix project.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            beam.interpreters.erlang_27
+            beam.packages.erlang_27.elixir_1_16
+            nodePackages.nodejs
+            flyctl
+          ]
+          ++ lib.optionals stdenv.isLinux [
+            # For ExUnit Notifier on Linux.
+            libnotify
+
+            # For file_system on Linux.
+            inotify-tools
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            # For ExUnit Notifier on macOS.
+            terminal-notifier
+
+            # For file_system on macOS.
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+          ];
+
+          shellHook = ''
+            # allows mix to work on the local directory
+            mkdir -p .nix-mix
+            mkdir -p .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-hex
+            export ERL_LIBS=$HEX_HOME/lib/erlang/lib
+
+            # concats PATH
+            export PATH=$MIX_HOME/bin:$PATH
+            export PATH=$MIX_HOME/escripts:$PATH
+            export PATH=$HEX_HOME/bin:$PATH
+
+            # enables history for IEx
+            export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
+          '';
+        };
+      }
+    );
+}
